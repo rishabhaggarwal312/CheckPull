b301521a4c33301c1dd6092944b2ef18239a5ece
Rishab.Aggarwal <Rishab.Aggarwal@compass-894>
2021-09-09 16:39:22 +0530

    commitnew

--- Check/src/main/java/lkllj/kljkj.java
+++ Check/src/main/java/lkllj/kljkj.java
@@ -4,7 +4,9 @@ public class kljkj {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Manisha Ji manisha manisha manisha is pagal  
+		//Manisha Ji manisha manisha manisha is pagal 
+		
+		// Rishabh is Mad About you
 
 	} 
 
